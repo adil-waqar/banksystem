23010e7f3dba846b61b89fc067a9b0dbe210734d
adil-waqar <adwiq78@gmail.com>
2018-04-18 12:14:41 +0500

    Added _inc and changes in datastructs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-#include "datastructs.h"
+#include "_inc/datastructs.h"
 
 int main(){
+  bst avl1;
+  bst* root = NULL;
+  root = avl1.insert(2, root);
+  root = avl1.insert(5, root);
+  root = avl1.insert(7, root);
+  root = avl1.remove(root, 5);
+  avl1.display(root);
 
 }

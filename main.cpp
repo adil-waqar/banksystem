fd973d31df322edde3a8389e47638c4186efd432
adil-waqar <adwiq78@gmail.com>
2018-04-18 12:19:54 +0500

    Try

--- main.cpp
+++ main.cpp
@@ -4,12 +4,5 @@ using namespace std;
 #include "_inc/datastructs.h"
 
 int main(){
-  bst avl1;
-  bst* root = NULL;
-  root = avl1.insert(2, root);
-  root = avl1.insert(5, root);
-  root = avl1.insert(7, root);
-  root = avl1.remove(root, 5);
-  avl1.display(root);
 
 }

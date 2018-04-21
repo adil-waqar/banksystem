605d4173ae0688c8c4d35e0659514e26f8d4a944
adil-waqar <adwiq78@gmail.com>
2018-04-21 12:39:38 +0500

    Path

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-#include "E:\CS221\_inc\datastructs.h"
+#include "E:\CS221\_inc\datastructs.h" // Add your own path here
 
 
 int main(){
-  
+
 }
